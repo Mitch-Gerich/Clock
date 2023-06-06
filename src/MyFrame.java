cdc3d886403c1414ad17a585079dfd8b2a9d8edf
Mitchell Gerich <mitchgerich@Mitchs-MBP.lan>
2023-06-06 10:28:44 -0400

    Initial commit

---
+++ src/MyFrame.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyFrame extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+
+    SimpleDateFormat dayFormat;
+
+    SimpleDateFormat dateFormat;
+    JLabel timeLabel;
+
+    JLabel dayLabel;
+
+    JLabel dateLabel;
+
+
+    String time;
+
+    String day;
+
+    String date;
+
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Clock Program");
+        this.setLayout(new FlowLayout());
+        this.setSize(350,200);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
+
+
+        timeLabel = new JLabel();
+
+        dayLabel = new JLabel();
+
+        dateLabel = new JLabel();
+
+
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+
+        setTime();
+
+    }
+    public void setTime() {
+        while(true) {
+        time = timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+
+        day = dayFormat.format(Calendar.getInstance().getTime());
+        dayLabel.setText(day);
+
+        date = dateFormat.format(Calendar.getInstance().getTime());
+        dateLabel.setText(date);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        }
+
+    }
+}
